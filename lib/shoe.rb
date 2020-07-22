36b1e819be1ba8a1e6d0aa2db91ac438c54ae22b
Denise Henderson <denise@henderson3.us>
2020-07-22 18:49:40 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,10 +1,15 @@
 # Make your shoe class here!
 class shoe
 
+  attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
   def initialize(brand)
     @brand = brand
+  end
+
+  def cobble
+    @shoe = shoe.new
   end
 
 end
